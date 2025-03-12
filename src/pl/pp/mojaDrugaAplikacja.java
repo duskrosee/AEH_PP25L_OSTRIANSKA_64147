26204381cfd815c924197b04078e8c0248f29e54
umbraya <serpente.mari@gmail.com>
2025-03-12 22:29:53 +0100

    Zapisy do repozytorium Lab2 (1, 2, 3)

--- src/pl/pp/mojaDrugaAplikacja.java
+++ src/pl/pp/mojaDrugaAplikacja.java
@@ -1,5 +1,44 @@
 package pl.pp;
 
+import java.util.Scanner;
+
+public class mojaDrugaAplikacja {
+    public static void main(String[] args) {
+
+        double x = 10;
+        double y = 2;
+
+        Scanner scanner = new Scanner(System.in);
+
+        var result = x + y;
+        System.out.println("x + y = " + result);
+
+        result = x - y;
+        System.out.println("x - y = " + result);
+
+        result = x * y;
+        System.out.println("x * y = " + result);
+
+        result = x / y;
+        System.out.println("x / y = " + result);
+
+        result = x % y;
+        System.out.println("x % y = " + result);
+
+        System.out.println("Wpisz dwie liczby rozdzielone klawiszem Enter:");
+        double first = scanner.nextDouble();
+        double second = scanner.nextDouble();
+
+        System.out.println("x + y = " + (first + second));
+    }
+}
+
+
+
+
+
+package pl.pp;
+
 public class mojaDrugaAplikacja {
     public static void main(String[] args) {
 
@@ -11,5 +50,28 @@ public class mojaDrugaAplikacja {
 
         int xSquared = x * x;
         System.out.println("x^2 = " + xSquared);
+    }
+}
+
+
+
+
+package pl.pp;
+
+import java.util.Scanner
+
+public class mojaDrugaAplikacja {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Proszę podaj pełny wiek w latach:");
+        int ageInYears = scanner.nextInt();
+
+        long ageInSeconds = ageInYears * 31536000;
+
+        System.out.println("Oto podany wiek w przelczeniu na sekundy: " + ageInSeconds);
+
+        scanner.close();
     }
 }
